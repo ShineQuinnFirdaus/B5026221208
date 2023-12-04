f99023249a75bf0402c91a5cf606487da239845b
ShineQuinnFirdaus <quinn.shine@gmail.com>
2023-12-05 03:06:07 +0700

    Tugas pertemuan tanggal 28 Nop

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -1,50 +1,47 @@
 @extends('master')
 
-@section('title', 'Data Pegawai')
+@section('title','Edit Pegawai')
 
 @section('konten')
-	<h2><a href="https://www.malasngoding.com">www.malasngoding.com</a></h2>
-	<h3>Edit Pegawai</h3>
 
-	<a href="/pegawai" class="btn btn-primary"> Kembali</a>
+    <h2><a href="https://www.malasngoding.com">www.malasngoding.com</a></h2>
+    <h3>Edit Pegawai</h3>
 
-	<br/>
-	<br/>
+    <a href="/pegawai" class="btn btn-primary"> Kembali</a>
 
-	@foreach($pegawai as $p)
-	<form action="/pegawai/update" method="post">
-		{{ csrf_field() }}
-		<input type="hidden" name="id" value="{{ $p->pegawai_id }}"> <br/>
-		<div class="form-group">
-            <label for="nama" class="col-xs-3 col-form-label mr-2">Nama</label>
-            <div class="col-xs-9">
-            <input type="text" class="form-control" required="required" id="nama" name="nama" placeholder="Masukan nama pegawai" value="{{ $p->pegawai_nama }}">
-            </div>
+    <br/>
+    <br/>
+
+    <form action="/pegawai/store" method="post" class="form-horizontal">
+        {{ csrf_field() }}
+        <div class="form-group">
+            <table style="width: 100%;">
+                <tr>
+                    <td style="width: 30%;"><label for="nama" class="col-xs-3 col-form-label mr-2">Nama</label></td>
+                    <td style="width: 70%;"><div class="col-xs-9">
+                        <input type="text" class="form-control" id="nama" name="nama">
+                    </div></td>
+                </tr>
+                <tr>
+                    <td><label for="jabatan" class="col-xs-3 col-form-label mr-2">Jabatan</label></td>
+                    <td><div class="col-xs-9">
+                        <input type="text" class="form-control" id="jabatan" name="jabatan">
+                    </div></td>
+                </tr>
+                <tr>
+                    <td><label for="umur" class="col-xs-3 col-form-label mr-2">Umur</label></td>
+                    <td><div class="col-xs-9">
+                        <input type="number" class="form-control" id="umur" name="umur" placeholder="Masukkan umur pegawai" required="required">
+                    </div></td>
+                </tr>
+                <tr>
+                    <td><label for="alamat" class="col-xs-3 col-form-label mr-2">Alamat</label></td>
+                    <td><div class="col-xs-9">
+                        <textarea type="text" class="form-control" id="alamat" name="alamat" placeholder="Masukkan alamat pegawai" required="required"></textarea>
+                    </div></td>
+                </tr>
+            </table>
         </div>
-
-		<div class="form-group">
-            <label for="nama" class="col-xs-3 col-form-label mr-2">Jabatan</label>
-            <div class="col-xs-9">
-            <input type="text" class="form-control" id="jabatan" name="jabatan" placeholder="Masukan jabatan pegawai" required="required">
-            </div>
-        </div>
-
-
-		<div class="form-group">
-            <label for="nama" class="col-xs-3 col-form-label mr-2">Umur</label>
-            <div class="col-xs-9">
-            <input type="number" class="form-control" id="umur" name="umur" placeholder="Masukan umur pegawai" value="{{ $p->pegawai_umur }}" required="required">
-            </div>
-        </div>
-
-		<div class="form-group">
-            <label for="nama" class="col-xs-3 col-form-label mr-2">Alamat</label>
-            <div class="col-xs-9">
-            <textarea type="text" class="form-control" id="alamat" name="alamat" placeholder="Masukan alamat pegawai" required="required">{{ $p->pegawai_alamat }}</textarea>
-            </div>
-        </div>
-		<input type="submit" value="Simpan Data" class="btn btn-success">
-	</form>
-	@endforeach
-	@endsection
-
+        <input type="submit" value="Simpan Data" class="btn btn-success">
+    </form>
+@endsection
--- resources/views/tambah.blade.php
+++ resources/views/tambah.blade.php
@@ -4,44 +4,44 @@
 
 @section('konten')
 
-	<h2><a href="https://www.malasngoding.com">www.malasngoding.com</a></h2>
-	<h3>Data Pegawai</h3>
+    <h2><a href="https://www.malasngoding.com">www.malasngoding.com</a></h2>
+    <h3>Data Pegawai</h3>
 
     <a href="/pegawai" class="btn btn-primary"> Kembali</a>
 
-	<br/>
-	<br/>
+    <br/>
+    <br/>
 
-	<form action="/pegawai/store" method="post" class="form-horizontal">
-		{{ csrf_field() }}
+    <form action="/pegawai/store" method="post" class="form-horizontal">
+        {{ csrf_field() }}
         <div class="form-group">
-            <table>
-            <label for="nama" class="col-xs-3 col-form-label mr-2">Nama</label>
-            <div class="col-xs-9">
-            <input type="text" class="form-control" id="nama" name="nama">
-            </div>
-
-        <div class="form-group">
-            <label for="nama" class="col-xs-3 col-form-label mr-2">Jabatan</label>
-            <div class="col-xs-9">
-            <input type="text" class="form-control" id="nama" name="nama">
-            </div>
+            <table style="width: 100%;">
+                <tr>
+                    <td style="width: 30%;"><label for="nama" class="col-xs-3 col-form-label mr-2">Nama</label></td>
+                    <td style="width: 70%;"><div class="col-xs-9">
+                        <input type="text" class="form-control" id="nama" name="nama">
+                    </div></td>
+                </tr>
+                <tr>
+                    <td><label for="jabatan" class="col-xs-3 col-form-label mr-2">Jabatan</label></td>
+                    <td><div class="col-xs-9">
+                        <input type="text" class="form-control" id="jabatan" name="jabatan">
+                    </div></td>
+                </tr>
+                <tr>
+                    <td><label for="umur" class="col-xs-3 col-form-label mr-2">Umur</label></td>
+                    <td><div class="col-xs-9">
+                        <input type="number" class="form-control" id="umur" name="umur" placeholder="Masukkan umur pegawai" required="required">
+                    </div></td>
+                </tr>
+                <tr>
+                    <td><label for="alamat" class="col-xs-3 col-form-label mr-2">Alamat</label></td>
+                    <td><div class="col-xs-9">
+                        <textarea type="text" class="form-control" id="alamat" name="alamat" placeholder="Masukkan alamat pegawai" required="required"></textarea>
+                    </div></td>
+                </tr>
+            </table>
         </div>
-
-        <div class="form-group">
-            <label for="first_name" class="col-xs-3 col-form-label mr-2">Umur</label>
-            <div class="col-xs-9">
-            <input type="number" class="form-control" id="umur" name="umur" placeholder="Masukan umur pegawai" required="required">
-            </div>
-        </div>
-
-        <div class="form-group">
-            <label for="first_name" class="col-xs-3 col-form-label mr-2">Alamat</label>
-            <div class="col-xs-9">
-            <textarea type="text" class="form-control" id="alamat" name="alamat" placeholder="Masukan alamat pegawai" required="required"></textarea>
-            </div>
-        </div>
-		<input type="submit" value="Simpan Data" class="btn btn-success">
-
-	</form>
+        <input type="submit" value="Simpan Data" class="btn btn-success">
+    </form>
 @endsection
